f6179271d64916e831d6fec768aeb2aa1b789a91
Sok4h <55671660+Sok4h@users.noreply.github.com>
2020-11-26 23:27:24 -0500

    My Plant + Add New Plant

---
+++ app/src/main/java/com/sokah/leafmate/AddNewPlantActivity.java
@@ -0,0 +1,14 @@
+package com.sokah.leafmate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNewPlantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_new_plant);
+    }
+}
