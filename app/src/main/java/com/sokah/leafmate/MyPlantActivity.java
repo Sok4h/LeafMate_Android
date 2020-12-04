1273c2ce7f7031d9727a86de5223973370f767a2
Sok4h <55671660+Sok4h@users.noreply.github.com>
2020-12-04 00:57:22 -0500

    actualizacion diseño + intento cargar mi planta en la actividad

--- app/src/main/java/com/sokah/leafmate/MyPlantActivity.java
+++ app/src/main/java/com/sokah/leafmate/MyPlantActivity.java
@@ -2,13 +2,62 @@ package com.sokah.leafmate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MyPlantActivity extends AppCompatActivity {
 
+    TextView title,age,type,tip,sunLight;
+    ImageView imagePlant;
+    Button water;
+    ImageButton back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_plant);
+
+        Plant plant = (Plant) getIntent().getSerializableExtra("plant");
+        title=findViewById(R.id.titleMyPlant);
+        age=findViewById(R.id.ageMyPlant);
+        type=findViewById(R.id.typeMyPlant);
+        imagePlant=findViewById(R.id.imageMyPlant);
+        water=findViewById(R.id.btnWaterMyPlant);
+        sunLight=findViewById(R.id.sunLightMyPlant);
+        back=findViewById(R.id.btnBackMyPlant);
+
+        water.setOnClickListener(
+                (v)->{
+
+                    plant.Water();
+
+                }
+        );
+
+        back.setOnClickListener(
+                (v)->{
+
+                    finish();
+                }
+        );
+
+        title.setText(plant.getName());
+        type.setText(plant.getType());
+        sunLight.setText(plant.getSunLight());
+
+        switch (plant.getName()){
+
+            case "Cilantro":
+
+                imagePlant.setBackgroundResource(R.drawable.cilantro);
+
+                break;
+
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/sokah/leafmate/Plant.java
@@ -0,0 +1,73 @@
+package com.sokah.leafmate;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Plant implements Serializable {
+
+    private String id,name, userName,type,sunLight;
+    private Date bornDate;
+
+
+    public Plant(String id, String name,String userName, String type,String sunLight,Date bornDate) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.sunLight=sunLight;
+        this.bornDate=bornDate;
+        this.userName=userName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getSunLight() {
+        return sunLight;
+    }
+
+    public Date getBornDate() {
+        return bornDate;
+    }
+
+    public void setBornDate(Date bornDate) {
+        this.bornDate = bornDate;
+    }
+
+    public void setSunLight(String sunLight) {
+        this.sunLight = sunLight;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void Water(){
+
+
+    }
+}
